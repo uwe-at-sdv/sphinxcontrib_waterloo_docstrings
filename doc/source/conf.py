dfddaec251b50d597ae3a4e8e0863ef76af4af05
Uwe <uwe@sci-d-vis.com>
2026-07-17 16:45:05 +0200

    Sphinx branch: 0.5.2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -52,7 +52,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
 
 html_theme_options = {
@@ -73,7 +73,7 @@ html_favicon = '_static/favicon.ico'
 html_static_path = ['_static']
 
 html_css_files = [
-	'waterloo_alabaster.css',
+#	'waterloo_alabaster.css',
 	'wtrl_doc_custom.css',
 	]
 
